00dc70a3a8f82921330f2739ffd4d79dedeb2317
super117 <mphon8422@gmail.com>
2021-01-05 22:03:09 +0800

    Added Home Content Image

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,32 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
+
+const StyledHome = styled.div`
+	display: flex;
+	justify-content: center;
+	margin-left: auto;
+	margin-right: auto;
+	max-width: 1500px;
+`;
+
+const StyledHomeImage = styled.img`
+	width: 100%;
+	z-index: -1;
+	mask-image: linear-gradient(to bottom, rgba(0, 0, 0, 1), rgba(0, 0, 0, 0));
+`;
 
 class Header extends Component {
 
 	render() {
 		return (
-			<div>Home</div>
+			<StyledHome>
+				<div>
+					<StyledHomeImage
+						src="https://images-na.ssl-images-amazon.com/images/G/01/sm/Gateway/Dec2020_testing/wk3_GW1_fallguys_1500x600_EN_v2._CB413781238_.jpg"
+						alt=""
+					/>
+				</div>
+		  </StyledHome>
 		);
 	}
 }
