86885e069d0f91cd386478b13e2a3c5e988258d2
super117 <mphon8422@gmail.com>
2021-01-05 23:39:53 +0800

    Added Product Items

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
+import Products from './Products.js';
 
 const StyledHome = styled.div`
 	display: flex;
@@ -7,10 +8,13 @@ const StyledHome = styled.div`
 	margin-left: auto;
 	margin-right: auto;
 	max-width: 1500px;
+	height: 300px;
+	overflow: visible;
 `;
 
 const StyledHomeImage = styled.img`
 	width: 100%;
+	height: 600px;
 	z-index: -1;
 	mask-image: linear-gradient(to bottom, rgba(0, 0, 0, 1), rgba(0, 0, 0, 0));
 `;
@@ -19,14 +23,15 @@ class Header extends Component {
 
 	render() {
 		return (
-			<StyledHome>
-				<div>
+			<div>
+				<StyledHome>
 					<StyledHomeImage
 						src="https://images-na.ssl-images-amazon.com/images/G/01/sm/Gateway/Dec2020_testing/wk3_GW1_fallguys_1500x600_EN_v2._CB413781238_.jpg"
 						alt=""
 					/>
-				</div>
-		  </StyledHome>
+				</StyledHome>
+				<Products />
+			</div>
 		);
 	}
 }
---
+++ src/components/Products.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import ProductItem from '../utils/ProductItem.js';
+
+const StyledProducts = styled.div`
+    padding: 0 10px 0;
+`;
+
+const StyledProductsContent = styled.div`
+    max-width: 1480px;
+    margin: 0 auto;
+    overflow: hidden;
+    padding: 20px 0;
+`;
+
+const StyledRow = styled.div`
+    display: flex;
+`;
+
+class Products extends Component {
+
+	render() {
+		return (
+			<StyledProducts>
+				<StyledProductsContent>
+                    <StyledRow>
+                        <ProductItem 
+                            title="Fitness for the new yeaer"
+                            img="https://images-na.ssl-images-amazon.com/images/G/01/US-hq/2020/img/Events/XCM_CUTTLE_1302372_1541708_US_NYNY_3630094_379x304_1X_en_US._SY304_CB413096725_.jpg"
+                            footer="Shop Home Finess"
+                        />
+                        <ProductItem 
+                            title="Book for the new year"
+                            img="https://images-na.ssl-images-amazon.com/images/G/01/US-hq/2020/img/Events/XCM_CUTTLE_1302372_1541707_US_NYNY_3630091_379x304_1X_en_US._SY304_CB413096725_.jpg"
+                            footer="See more books"
+                        />
+                        <ProductItem 
+                            title="All-new Ech Frames"
+                            img="https://images-na.ssl-images-amazon.com/images/G/01/kindle/journeys/MWQ0NzdiNzQt/MWQ0NzdiNzQt-NDFlZjdlZDIt-w379._SY304_CB412115001_.jpg"
+                            footer="Learn more"
+                        />
+                        <ProductItem 
+                            title="Deal of the Day"
+                            img="https://m.media-amazon.com/images/I/41FHFmuc21L._AC_SY240_.jpg"
+                            footer="See all deals"
+                        />
+                    </StyledRow>
+                </StyledProductsContent>
+			</StyledProducts>
+		);
+	}
+}
+
+export default Products;
